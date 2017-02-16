f524c771236aa08d2120e942a9a1d051cb639ae8
Nicholas Cai <ncai@berkeley.edu>
2017-02-15 22:50:15 -0800

    nick test 2

--- todolist.py
+++ todolist.py
@@ -231,6 +231,8 @@ class ToDoListMDP(mdp.MarkovDecisionProcess):
         self.todolist = todolist
         self.state = (tasks, time)
 
+        asdf
+
         # parameters
         self.livingReward = 0.0
         self.noise = 0.0
